195b24e50929b1f52bb417f737259f0b7240edc4
jk at orin <juikun.chen@hpinc.com>
2025-02-05 13:24:47 -0500

    leetcode 1

--- cpp/1_two_sum.cpp
+++ cpp/1_two_sum.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
@@ -10,20 +10,21 @@ class Solution {
 };
 
 std::vector<int> Solution::twoSum(const vector<int>& nums, int target) {
-    std::map<int, int> m;
+    std::unordered_map<int, int> m;
     for (int i=0; i<nums.size(); i++) {
-        if (m.count(target-nums[i])) {
-            return {i, m[target-nums[i]]};
-        } else {
-            m[nums[i]] = i;
+        int diff = target - nums[i];
+        if (m.contains(diff)) {
+            return {i, m[diff]};
         }
+        m[nums[i]] = i;
     }
     return {};
 }
 
 int main() {
-  std::vector<int> result = Solution::twoSum(std::vector<int>{1,3,5,7,9}, 8);
+  std::vector<int> result = Solution::twoSum(std::vector<int>{1,3,5,7,9}, 12);
   for (int index:result) {
     std::cout << index << std::endl;
   }
 }
+// g++ 1_two_sum.cpp -std=c++2a
